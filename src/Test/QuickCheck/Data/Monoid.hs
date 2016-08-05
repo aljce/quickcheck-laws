64cbd2f23c186a8118a5931daef6bdb18d886ea4
Kyle McKean <mckean.kylej@gmail.com>
2016-08-05 12:58:18 -0400

    Data.Monoid Laws

---
+++ src/Test/QuickCheck/Data/Monoid.hs
@@ -0,0 +1,17 @@
+module Test.QuickCheck.Data.Monoid where
+
+import Test.QuickCheck
+import Test.QuickCheck.Laws
+
+import Data.Monoid
+
+checkMonoid :: forall m. (Monoid m, Arbitrary m, Eq m, Show m) => Proxy m -> TestGroup
+checkMonoid _ = TestGroup "Monoid" [Named "Left Identity" leftIdentity,
+                                    Named "Right Identity" rightIdentity,
+                                    Named "Associativity" associativity]
+  where leftIdentity  :: m -> Bool
+        leftIdentity  x = (mempty <> x) == x
+        rightIdentity :: m -> Bool
+        rightIdentity x = (x <> mempty) == x
+        associativity :: m -> m -> m -> Bool
+        associativity x y z = x <> (y <> z) == (x <> y) <> z
--- src/Test/QuickCheck/Laws.hs
+++ src/Test/QuickCheck/Laws.hs
@@ -1,1 +1,37 @@
-module Test.QuickCheck.Laws where
+{-# LANGUAGE GeneralizedNewtypeDeriving, GADTs, ExistentialQuantification #-}
+module Test.QuickCheck.Laws (A
+                            ,B
+                            ,C
+                            ,D
+                            ,E
+                            ,NamedProp(..)
+                            ,TestGroup(..)
+                            ,module X
+                            )where
+
+import Data.Semigroup
+import Test.QuickCheck
+import Data.Text
+import qualified Data.Text.IO as T
+import Data.Proxy as X
+
+newtype A = A Ordering deriving (Eq,Ord,Bounded,Show,Semigroup,Monoid,Arbitrary,CoArbitrary)
+newtype B = B Ordering deriving (Eq,Ord,Bounded,Show,Semigroup,Monoid,Arbitrary,CoArbitrary)
+newtype C = C Ordering deriving (Eq,Ord,Bounded,Show,Semigroup,Monoid,Arbitrary,CoArbitrary)
+newtype D = D Ordering deriving (Eq,Ord,Bounded,Show,Semigroup,Monoid,Arbitrary,CoArbitrary)
+newtype E = E Ordering deriving (Eq,Ord,Bounded,Show,Semigroup,Monoid,Arbitrary,CoArbitrary)
+
+data NamedProp where
+  Named :: (Testable p) => Text -> p -> NamedProp
+
+data TestGroup where
+  TestGroup :: (Foldable f) => Text -> f NamedProp -> TestGroup
+
+testGroups :: (Foldable f) => f TestGroup -> IO ()
+testGroups = mapM_ testGroup
+  where testGroup (TestGroup groupName props) = do
+          T.putStrLn $ "Testing group: " <> groupName
+          mapM_ check props
+        check (Named name prop) = do
+          T.putStrLn $ "  Prop: " <> name
+          quickCheck prop
